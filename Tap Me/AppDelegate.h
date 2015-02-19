72f905154af2de97dd4bb04fc8ac62b335dbe359
Khairul <kyu.helf@gmail.com>
2015-02-19 10:29:22 +0700

    Initial Commit

---
+++ Tap Me/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tap Me
+//
+//  Created by Khairul  on 2/19/15.
+//  Copyright (c) 2015 Me Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
